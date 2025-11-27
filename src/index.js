9cde9ed2bf91048f848ded81b2bbe57b8f6e7b42
rrsc-dev <reis.rrsc@outlook.com>
2025-11-27 18:04:06 -0300

    informações e desinstalação de pacote

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 // node --watch index.js -> reinicia a aplicação sempre que houver mudanças no arquivo
 // npm i pretty-console-logs -D -> instala a dependência apenas para desenvolvimento
 // npm uninstall pretty-console-logs -> desinstala a dependência
+// npm fund *nome do package -> lista a página com os arquivos das dependências do projeto e suas versões, o *nome do package é opcional
+// npm update *nome do package -> atualiza todas as dependências do projeto
 
 import chalk from "chalk";
 import logSymbols from "log-symbols";
