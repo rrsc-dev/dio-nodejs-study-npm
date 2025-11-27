fdb102b66e66db231a522c46eb6e528211b61fd3
rrsc-dev <reis.rrsc@outlook.com>
2025-11-27 17:38:47 -0300

    instalação de package em desenvolvimento

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 // npm i -> instala as dependências listadas no package.json
 // npm init -y -> cria um package.json com as configurações padrão
 // npm init -> cria um package.json com perguntas para configurar o arquivo
+// node --watch index.js -> reinicia a aplicação sempre que houver mudanças no arquivo
+// npm i pretty-console-logs -D -> instala a dependência apenas para desenvolvimento
+
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+console.log(logSymbols.success, chalk.red.bgBlue.italic.bold("Hello world!"));
+console.log(
+  logSymbols.error,
+  chalk.red.bgRedBright.italic.bold("Hello world!")
+);
