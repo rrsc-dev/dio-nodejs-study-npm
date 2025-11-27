bf3d35005eb1f245d4aa90ebf5e093c6b0cb484f
rrsc-dev <reis.rrsc@outlook.com>
2025-11-27 17:19:07 -0300

    comentários no index

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// npm i -> instala as dependências listadas no package.json
+// npm init -y -> cria um package.json com as configurações padrão
+// npm init -> cria um package.json com perguntas para configurar o arquivo
